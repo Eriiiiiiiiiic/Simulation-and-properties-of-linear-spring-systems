d66a46835db401946eb4829820ac9ab2c03f2e72
Eriiiiiiiiiic <91080225+Eriiiiiiiiiic@users.noreply.github.com>
2022-08-19 14:06:43 +0200

    kleine Buggs im Energie Plot geändert

--- main.m
+++ main.m
@@ -2,29 +2,30 @@ function main
 
 
 % COBWEB
-netz = 3;
-int_method = 2;
-energy_plot = 0;
+netz = 2;
+int_method = 1;
+energy_plot = 1;
 
+%Spinnennetz
 if netz == 1
 %%{
-  nc = 15;
-  c = 7;
+  nc = 5;
+  c = 3;
 
   fixpoints = [nc*c+2 - nc: nc*c+1];
-  d = 0.1;
+  d = 0.2;
   k = 1;  %Federkonstante
   m = 1;  %Masse
   g = 0.01;  #Gravitation
   friction = 1;  %Reibungskoeffizient
   verb1 = connections(nc,c);
-  p_0 = [0,0,0,0];
+  p_0 = [0,0,0.5,-0.5];
   C1 = matrix(nc,c,p_0);
   C2 = realistic_cobweb(d,verb1,C1){1};
   C3(1:2,:) = C2;
   P = [transpose(p_0(3:4)),zeros(2,length(C2)-1)];
   C3(3:4,:) = P;
-  verb2 = realistic_cobweb(nc,c,d,verb1,C1){2};  
+  verb2 = realistic_cobweb(d,verb1,C1){2};  
   verb = imp_verb(verb2);
   DistMat = dist_neu(length(C3),C3,verb);
   length(DistMat);
@@ -33,15 +34,32 @@ if netz == 1
   C = stretch(C3,s);
 %}
 endif
-if netz == 2
-
 
 % HÄNGENDE KETTE
-
-%{
+if netz == 2
+  %%{
   n = 10;
   fixpoints = [1,n];
-  k = 5;  %Federkonstante
+  k = 1;  %Federkonstante
+  m = 1;  %Masse
+  g = 1;  #Gravitation
+  friction = 0;  %Reibungskoeffizient
+  verb = {{2}};
+  for i = 2:(n-1)
+    verb{i}{1} = i-1;
+    verb{i}{2} = i+1;
+  end
+  verb{n}{1} = n-1;
+  C = [(1/n)* 1:n; 1:n; zeros(1,n); zeros(1,n)];
+  DistMat = dist_neu(n,C,verb);
+  %}
+endif
+% LIANE
+if netz == 3
+%%{
+  n = 20;
+  fixpoints = [1];
+  k = 10;  %Federkonstante
   m = 1;  %Masse
   g = 10;  #Gravitation
   friction = 0;  %Reibungskoeffizient
@@ -52,14 +70,11 @@ if netz == 2
   end
   verb{n}{1} = n-1;
   C = [(1/n)* 1:n; 1:n; zeros(1,n); zeros(1,n)];
-  DistMat = dist_neu(n,C);
+  DistMat = dist_neu(n,C,verb);
 %}
 endif
-if netz == 3
-
-
 % Gitter
-
+if netz == 4
 %%{
   h = 4;
   w = 4;
@@ -90,20 +105,26 @@ if netz == 3
 endif
 
 
+  
+  
+  
   gamma = 0.5;
   beta = 0.25;
+  
+  tau = 1/16;
+  T = 20;
+  framerate = 1 % jedes ***-te Bild wird gezeichnet
+  timesteps = floor(T/tau)
 
+  
   X_initial = transpose(transpose(C)(:));
   X = X_initial; % Zeilenmatrix
-  tau = 1/16;
-  T = 200;
-  timesteps = floor(T/tau);
-
-
+  
   positions_stored = zeros(2*n,timesteps);
 
-  clf;
 
+  figure(2)
+  clf;
   for t = 1:timesteps
     t
     if int_method == 1
@@ -149,10 +170,10 @@ endif
       iter += 1;
     until(iter > 50 || error < 10**(-3))
 
-    endif
 
     %X aus x_new,v_new berechnen
     X = [x_new,v_new];
+    endif
 
     %}
 
@@ -163,50 +184,52 @@ endif
 
 
     if energy_plot == 1
-    %%{
-    if t!=1
-      energy_temp = energy;
-      kin_energy_temp = kin_energy;
-      spannungs_energy_temp  = spannungs_energy;
-      potentielle_energy_temp = potentielle_energy;
-    end
-    energy = 0;
-    kin_energy = 0;
-    spannungs_energy = 0;
-    potentielle_energy = 0;
-    for i = 1:n
-      kin_energy += 1/2 * m * norm([X(1,2*n+i),X(1,3*n+i)])^2; #kinetische Energie
-      for j = cell2mat(verb{i})
-        spannungs_energy += 1/4 * k * (norm([X(1,i)-X(1,j),X(1,n+i)-X(1,n+j)]) - DistMat(i,j))^2; #spann/potentielle Ernergie. Der Fakotr 1/4 kommt davon, dass die Federn doppelt gezählt werden.
+      %%{
+      if t!=1
+        energy_temp = energy;
+        kin_energy_temp = kin_energy;
+        spannungs_energy_temp  = spannungs_energy;
+        potentielle_energy_temp = potentielle_energy;
       end
-      potentielle_energy += m*g*X(1,n+i);
-    end
-    energy = kin_energy + spannungs_energy + potentielle_energy;
-    figure(1);
-    if t!=1
-      plot([t/timesteps,(t+1)/timesteps],[energy_temp,energy],'r-'); hold on;
-      plot([t/timesteps,(t+1)/timesteps],[kin_energy_temp,kin_energy],'bo'); hold on;
-      plot([t/timesteps,(t+1)/timesteps],[spannungs_energy_temp,spannungs_energy],'g--'); hold on;
-      plot([t/timesteps,(t+1)/timesteps],[potentielle_energy_temp,potentielle_energy],'px'); hold on;
-      axis([0,1,0,100])
-      energy; #output im Befehlsfenster
-    end
-    %}
+      energy = 0;
+      kin_energy = 0;
+      spannungs_energy = 0;
+      potentielle_energy = 0;
+      for i = 1:n
+        kin_energy += 1/2 * m * 1/tau * norm([X(1,2*n+i),X(1,3*n+i)])^2; #kinetische Energie
+        for l = 1 : length(verb{i}) 
+          j = DistMat{i}{l}(1);% alle Verbindungen zu i
+          d = DistMat{i}{l}(2);
+          spannungs_energy += 1/4 * 1/tau * k * (norm([X(1,i)-X(1,j),X(1,n+i)-X(1,n+j)]) - d)^2; #spann/potentielle Ernergie. Der Fakotr 1/4 kommt davon, dass die Federn doppelt gezählt werden.
+        end
+        potentielle_energy += m*g*X(1,n+i);
+      end
+      energy = kin_energy + spannungs_energy + potentielle_energy;
+      if t!=1
+        %axis
+        xlabel ("rot: total energy, blau: kin, grün: spann, yellow: pot"); 
+        plot([t/timesteps,(t+1)/timesteps],[energy_temp,energy],'r-'); hold on;
+        plot([t/timesteps,(t+1)/timesteps],[kin_energy_temp,kin_energy],'bo'); hold on;
+        plot([t/timesteps,(t+1)/timesteps],[spannungs_energy_temp,spannungs_energy],'g--'); hold on;
+        plot([t/timesteps,(t+1)/timesteps],[potentielle_energy_temp,potentielle_energy],'yx'); hold on;
+        energy; #output im Befehlsfenster
+      end
+      %}
     endif
   end
 
 
   figHandle = figure(1);
-
   figure(1);
+
+  %axis([-1,9,-25,10]);
+  %axis([-3,3*w+1,-h,3*h+1]);
+  axis([0,5,0,5]);
   clf;
 
-  axis([-1,9,-25,10]);
-  %axis([-3,3*w+1,-h,3*h+1]);
-  %axis([-10,10,-10,10]);
+  
   for t = 1:timesteps
-    if rem(t,4) == 0
-      t
+    if rem(t,framerate) == 0
       clf;
       x = positions_stored(1:n,t); % die x Werte der n Punkte
       y = positions_stored(n+1:2*n,t);  % die y Werte der n Punkte
@@ -214,7 +237,7 @@ endif
         % Zeichnet die Punkte.
       %axis([-10,10,-10,10]);
       %axis([-3,2*w+1,-h,2*h+1]);
-      axis([-1,9,-25,10]);
+      axis([0,5,0,5]);
 
       for i = 1:n
         for j = cell2mat(verb{i})
@@ -222,7 +245,7 @@ endif
         end
       end
       drawnow;
-      MakeGif(figHandle, 'test.gif');
+      %MakeGif(figHandle, 'test.gif');
     end
   end
 end
